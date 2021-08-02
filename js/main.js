efc3ae117503bc2d72a12b63d2873b0da01e281c
haris-wpbrigade <87003725+haris-wpbrigade@users.noreply.github.com>
2021-08-02 05:12:41 -0700

    submenu toggle design
    bug on selecting a  submenu

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ ToggleBtn.addEventListener('click', ()=>{
     NavBar.classList.toggle('active')
     document.body.style.overflow = document.body.style.overflow === 'hidden' ? 'hidden scroll' : 'hidden'
 })
+
+const NavLinks = document.querySelectorAll('.Toggle-Submenu')
+const SubMenu = document.querySelectorAll('.Toggle-Submenu .submenu')
+
+for (let i=0; i<NavLinks.length; i++){
+    NavLinks[i].addEventListener('click', () => {
+        SubMenu[i].classList.toggle('active')
+    })
+}
